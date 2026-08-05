2a0a638897c06673f77d3771d35518457a86c171
Abhirup Banerjee <mailabhirupbanerjee@gmail.com>
2026-08-05 16:04:36 -0400

    gitbook error

--- src/lib/db/db-types.ts
+++ src/lib/db/db-types.ts
@@ -537,7 +537,7 @@ export interface FunctionApiCategoriesTable {
 
 export interface UserConnectedAccountsTable {
   id: string;
-  provider: Generated<'google' | 'microsoft' | 'github' | 'notion' | 'slack'>;
+  provider: Generated<'google' | 'microsoft' | 'github' | 'notion' | 'slack' | 'gitbook'>;
   user_email: string;
   display_name: string | null;
   access_token: string | null;
